19627d7cfb3745b3857ef97519006f374cc9a4d2
LieslMarie <Leedle001@hotmail.com>
2017-07-13 21:21:33 -0400

    Beardy Brews
    styles and changes made to project

--- js/app.js
+++ js/app.js
@@ -1,41 +1,8 @@
 $(document).foundation()
 
-//image carousel-->
+//sticky footer-->
 
 $( function () {
-  $(document).ready(function(){
-    $('.carousel').slick({
-      centerMode: true
-    });
-  });
-
-    $('.center').slick({
-    centerMode: true,
-    centerPadding: '60px',
-    slidesToShow: 6,
-    responsive: [
-      {
-        breakpoint: 768,
-        settings: {
-          arrows: false,
-          centerMode: true,
-          centerPadding: '40px',
-          slidesToShow: 6
-        }
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          arrows: false,
-          centerMode: true,
-          centerPadding: '40px',
-          slidesToShow: 6
-        }
-      }
-    ]
-  });
-
-    //sticky footer-->
 
     var height_diff = $( window ).height() - $( 'body' ).height();
     if ( height_diff > 0 ) {
