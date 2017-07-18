8f090354715a428f6da54fedaccd7a4c462ad56a
LieslMarie <Leedle001@hotmail.com>
2017-07-18 16:53:04 -0400

    Beardy Brews
    added JQuery

--- js/app.js
+++ js/app.js
@@ -2,11 +2,29 @@ $(document).foundation()
 
 //sticky footer-->
 
-$( function () {
+$(window).bind("load", function () {
+   var footer = $("#footer");
+   var pos = footer.position();
+   var height = $(window).height();
+   height = height - pos.top;
+   height = height - footer.height();
+   if (height > 0) {
+       footer.css({
+           'margin-top': height + 'px'
+       });
+   }
+});
 
-    var height_diff = $( window ).height() - $( 'body' ).height();
-    if ( height_diff > 0 ) {
-        $( '#footer' ).css( 'margin-top', height_diff + 50);
-    }
 
+function myfunction(){
+  alert("We will be in touch shortly!")
+};
+
+$(document).ready(function(){
+  $('li').mouseover(function(){
+    $(this).addClass('hover');
+  });
+    $('li').mouseout(function(){
+      $(this).removeClass('hover');
+  });
 });
